f8618267afbfecc888b0b13e89ff01ee3b63d863
Sunidhi Prasad <sunidhi15995@gmail.com>
2019-01-15 09:23:33 +0530

    Update RegexToFindHarry.java

--- main/java/RegexToFindHarry.java
+++ main/java/RegexToFindHarry.java
@@ -3,17 +3,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexToFindHarry {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a String: ");
-        String s = sc.nextLine();
-        int res=isHarry(s);
-        if(res==1)
-            System.out.println("Is Harry here ? true");
-        else
-            System.out.println("Is Harry here ? False");
-
-    }
 
     public static int isHarry(String s){
         String regex="Harry";
