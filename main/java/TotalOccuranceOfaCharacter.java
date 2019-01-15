7f4f0c212534625700d8e1a14d42c1d0c9e14d34
Sunidhi Prasad <sunidhi15995@gmail.com>
2019-01-15 09:24:39 +0530

    Update TotalOccuranceOfaCharacter.java

--- main/java/TotalOccuranceOfaCharacter.java
+++ main/java/TotalOccuranceOfaCharacter.java
@@ -1,18 +1,8 @@
 import java.util.Scanner;
 
 public class TotalOccuranceOfaCharacter {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a String: ");
-        String s = sc.nextLine();
-        System.out.print("Enter character to find it's occurance: ");
-        //char ch = sc.next().charAt(0);
-        String ch=sc.nextLine();
-        int res=diff(s,ch);
-        System.out.println("Occurance of "+ ch + ": "+ res);
-    }
 
-    public static int diff(String s, String ch){
+    public int diff(String s, String ch){
         int len = s.length();
         int fnal=0;
         if(ch.isEmpty()) {  return 0;}
