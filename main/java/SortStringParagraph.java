e4e6e14ff01c8a2b94e984a4aaa92cac38cc6619
Sunidhi Prasad <sunidhi15995@gmail.com>
2019-01-15 09:24:09 +0530

    Update SortStringParagraph.java

--- main/java/SortStringParagraph.java
+++ main/java/SortStringParagraph.java
@@ -2,15 +2,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class SortStringParagraph {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a String Paragraph: ");
-        String s=sc.nextLine();
-        String res=Sort(s);
-        System.out.println("Sorted Paragraph: "+ res );
-    }
-
-    public static String Sort(String S){
+  
+    public String Sort(String S){
         String s[]=S.split(" ");
         Arrays.sort(s);
         String res="";
